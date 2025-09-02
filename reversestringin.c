7a4729e20a5c503491738fd74f2828bec7dc8e92
Vandana SS <vandanavandy2005@gmail.com>
2025-09-02 22:21:47 +0530

    Add files via upload

---
+++ reversestringin.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+ void reverseString(char str[])
+ {
+ 	int left=0;
+ 	int right=strlen(str)-1;
+ 	while(left<right)
+ 	{
+ 		char temp=str[left];
+ 		str[left]=str[right];
+ 		str[right]=temp;
+ 		left++;
+ 		right--;
+ 		
+	 }
+ }
+ int main()
+ {
+ 	char str[]="HelloWorld";
+ 	reverseString(str);
+ 	printf("Reversed string:%s/n",str);
+ 	return 0;
+ }
